b3471bf144e8d1f8bec82a0ed8d0dbd6dff882bc
Dylan <d.barva5402@gmail.com>
2020-09-19 15:23:27 -0700

    more work on triangles

--- triangles.js
+++ triangles.js
@@ -10,10 +10,31 @@ function setup() {
 function draw() {
     clear()
     generate_triangle(document.getElementById("angle1").value, document.getElementById("angle2").value)
-    
-    // background(255)
 }
 
+function get_triangle_type(a1, a2, a3) {
+    let angles = [degrees(a1), degrees(a2), degrees(a3)]
+    var type = ""
+    var num_acute = 0
+    angles.forEach(function(item, index, array) {
+        if (Math.round(item) == 90) {
+            type = "Right"
+        } else if (item > 90) {
+            type = "Obtuse"
+        } else if (item < 90) {
+            num_acute += 1
+        }
+    })
+    if (num_acute == 3) {
+        type = "Acute"
+    }
+    if (angles[0] + angles[1] == 90) {
+        type = "Right"  
+    }
+    
+    return type
+
+}
 function generate_triangle(angle1, angle2) {
     var triangle_origin_x = 300
     var triangle_origin_y = 300
@@ -48,10 +69,10 @@ function generate_triangle(angle1, angle2) {
         console.log(triangle_last_y)
         triangle_last_y = triangle_origin_y - (side_b_length * Math.sin(angle2))
     }
-    fill("red")
+    fill("#accbf4")
     triangle(triangle_origin_x, triangle_origin_y, triangle_origin_x + base_side_length, triangle_origin_y, triangle_last_x, triangle_last_y)
     // console.log(degrees(angle1))
     // console.log(degrees(angle2))
-    console.log(triangle_last_y)
-
+    console.log(Math.round(degrees(angle3)))
+    document.getElementById("type").innerText = get_triangle_type(angle1, angle2, angle3)
 }
