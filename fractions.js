5928755bef97d96b0586113914842702a24e199c
duncanwedig <duncan.wedig@gmail.com>
2020-09-19 07:04:37 -0700

    fractions thing works now for real but is ugly

--- fractions.js
+++ fractions.js
@@ -4,12 +4,33 @@ const HEIGHT = 500;
 const BAR_WIDTH = 8*WIDTH/10;
 const BAR_HEIGHT = HEIGHT/10;
 
-var NUM_RECTS = 10;
-
-var RECT_WIDTH = BAR_WIDTH/NUM_RECTS;
 const RECT_HEIGHT = BAR_HEIGHT;
 const LEFT_RECT_EDGE = (WIDTH - BAR_WIDTH)/2;
 const TOP_RECT_EDGE = (HEIGHT - BAR_HEIGHT)/2;
+
+function bound(x, min, max) {
+    return x > min ? (x < max ? x : max) : min;
+}
+
+function isMouseOnCanvas() {
+    return mouseX > 0 && mouseX < WIDTH && mouseY > 0 && mouseY < HEIGHT;
+}
+
+function isMouseOnBar() {
+    return mouseX > LEFT_RECT_EDGE && mouseX < LEFT_RECT_EDGE + BAR_WIDTH &&
+        mouseY > TOP_RECT_EDGE && mouseY < TOP_RECT_EDGE + BAR_HEIGHT;
+}
+
+
+var numRects = 10;
+var filledRects = 5;
+
+var RECT_WIDTH = BAR_WIDTH/numRects;
+
+var circleGoalX = WIDTH/2;
+var circleX = WIDTH/2;
+const circleY = HEIGHT/2;
+var trackingMouse = false;
 
 function setup() {
     console.log("setup");
@@ -17,55 +38,72 @@ function setup() {
     // noStroke();
 }
 
-var circleGoalX = WIDTH/2;
-var circleX = WIDTH/2;
-const circleY = HEIGHT/2;
-
-function bound(x, min, max) {
-    return x > min ? (x < max ? x : max) : min;
+function draw() {
+    drawRects();
+    if (trackingMouse) {
+        updateCircleGoal();
+    } else {
+        updateFilledRects();
+    }
+    updateCircle();
 }
 
-function draw() {
+
+function mousePressed() {
+    console.log(isMouseOnBar());
+    if (isMouseOnBar()) {
+        trackingMouse = true;
+    }
+}
+
+function mouseReleased() {
+    if (trackingMouse) {
+        updateCircleGoal();
+    }
+    trackingMouse = false;
+
+}
+
+
+function drawRects() {
     updateNumRects();
     background(255);
     fill(255);
-    for (var i = 0; i < NUM_RECTS; i++) {
+    for (var i = 0; i < numRects; i++) {
         rect(LEFT_RECT_EDGE + RECT_WIDTH*i, TOP_RECT_EDGE, RECT_WIDTH, RECT_HEIGHT);
     }
     updateCircle();
 }
 
-function isMouseOnCanvas() {
-    return mouseX > 0 && mouseX < WIDTH && mouseY > 0 && mouseY < HEIGHT;
-}
 
 function updateCircleGoal() {
-    var numerator = Math.round((mouseX - LEFT_RECT_EDGE) / RECT_WIDTH);
-    document.getElementById("numerator").value = numerator;
-    circleGoalX = (numerator * RECT_WIDTH) + LEFT_RECT_EDGE;
-}
-
-function mouseReleased() {
-    if (isMouseOnCanvas()) {
-        updateCircleGoal();
-    }
-
+    filledRects = Math.round((mouseX - LEFT_RECT_EDGE) / RECT_WIDTH);
+    document.getElementById("numerator").value = bound(filledRects, 0, numRects);
+    circleGoalX = (filledRects * RECT_WIDTH) + LEFT_RECT_EDGE;
 }
 
 function updateFilledRects() {
-    var numerator = Math.round(document.getElementById("numerator").value);
-
-
+    filledRects = document.getElementById("numerator").value;
+    if (filledRects > numRects) {
+        document.getElementById("numerator").value = numRects;
+    } else if (filledRects < 0) {
+        document.getElementById("numerator").value = 0;
+    }
+    circleGoalX = (filledRects * RECT_WIDTH) + LEFT_RECT_EDGE;
 }
 
 function updateNumRects() {
-    if (NUM_RECTS != document.getElementById("denominator").value) {
-        NUM_RECTS = document.getElementById("denominator").value;
-        if (NUM_RECTS > BAR_WIDTH/10) {
+    if (numRects != document.getElementById("denominator").value) {
+        numRects = document.getElementById("denominator").value;
+        if (numRects > BAR_WIDTH/10) {
             document.getElementById("denominator").value = BAR_WIDTH/10;
-            NUM_RECTS = BAR_WIDTH/10;
+            numRects = BAR_WIDTH/10;
         }
-        RECT_WIDTH = BAR_WIDTH/NUM_RECTS;
+        if (numRects < 1) {
+            document.getElementById("denominator").value = 1;
+            numRects = 1;
+        }
+        RECT_WIDTH = BAR_WIDTH/numRects;
         updateCircleGoal();
     }
     
@@ -73,7 +111,7 @@ function updateNumRects() {
 }
 
 function updateCircle() {
-    if (mouseIsPressed == true) {
+    if (trackingMouse) {
         circleGoalX = bound(mouseX, LEFT_RECT_EDGE, LEFT_RECT_EDGE + BAR_WIDTH);
         // circleY = mouseY;
     }
