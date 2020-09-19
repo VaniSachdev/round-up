cacf5835cf4c2eedf48316f845996ab1dc4d2b4e
Dylan <d.barva5402@gmail.com>
2020-09-19 13:12:35 -0700

    triangle generation kinda sorta works

---
+++ triangles.js
@@ -0,0 +1,57 @@
+const WIDTH = 1000;
+const HEIGHT = 1000;
+
+function setup() {
+    console.log("setup");
+    createCanvas(WIDTH, HEIGHT);
+    // noStroke();
+}
+
+function draw() {
+    clear()
+    generate_triangle(document.getElementById("angle1").value, document.getElementById("angle2").value)
+    
+    // background(255)
+}
+
+function generate_triangle(angle1, angle2) {
+    var triangle_origin_x = 300
+    var triangle_origin_y = 300
+    var base_side_length = 400
+    var angle3 = radians(180 - (angle1 + angle2))
+    angle1 = radians(angle1)
+    angle2 = radians(angle2)
+    var side_b_length, max_height, triangle_last_x, triangle_last_y
+    triangle_last_y = -1
+    while (triangle_last_y < 0 || triangle_last_y > HEIGHT) {
+        side_b_length = (base_side_length * Math.sin(angle1))/Math.sin(angle3)
+        max_height = side_b_length * Math.sin(angle2);
+        triangle_last_x = triangle_origin_x - (side_b_length * Math.cos(angle2))
+        triangle_last_y = triangle_origin_y - (side_b_length * Math.sin(angle2))
+        base_side_length = base_side_length - 10
+    }
+    if (base_side_length < 100) {
+        base_side_length = 100
+        side_b_length = (base_side_length * Math.sin(angle1))/Math.sin(angle3)
+        max_height = side_b_length * Math.sin(angle2);
+        triangle_last_x = triangle_origin_x - (side_b_length * Math.cos(angle2))
+        triangle_last_y = triangle_origin_y - (side_b_length * Math.sin(angle2))
+        
+    }
+    if (triangle_last_x < 0 || triangle_last_x > 1000 || triangle_last_y < 0 || triangle_last_y > 1000) {
+        var temp = angle1
+        angle1 = angle2
+        angle2 = temp
+        side_b_length = (base_side_length * Math.sin(angle1))/Math.sin(angle3)
+        max_height = side_b_length * Math.sin(angle2);
+        triangle_last_x = triangle_origin_x - (side_b_length * Math.cos(angle2))
+        console.log(triangle_last_y)
+        triangle_last_y = triangle_origin_y - (side_b_length * Math.sin(angle2))
+    }
+    fill("red")
+    triangle(triangle_origin_x, triangle_origin_y, triangle_origin_x + base_side_length, triangle_origin_y, triangle_last_x, triangle_last_y)
+    // console.log(degrees(angle1))
+    // console.log(degrees(angle2))
+    console.log(triangle_last_y)
+
+}
