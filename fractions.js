396b0f9b000f95a15e98d1abea1ee661a8174b24
duncanwedig <duncan.wedig@gmail.com>
2020-09-19 07:06:24 -0700

    fixed the thing not showing up when not hosted on a live webserver

--- fractions.js
+++ fractions.js
@@ -4,29 +4,56 @@ const HEIGHT = 500;
 const BAR_WIDTH = 8*WIDTH/10;
 const BAR_HEIGHT = HEIGHT/10;
 
-const RECT_WIDTH = BAR_WIDTH/10;
+var NUM_RECTS = 10;
+
+var RECT_WIDTH = BAR_WIDTH/NUM_RECTS;
 const RECT_HEIGHT = BAR_HEIGHT;
 const LEFT_RECT_EDGE = (WIDTH - BAR_WIDTH)/2;
 const TOP_RECT_EDGE = (HEIGHT - BAR_HEIGHT)/2;
 
-void setup() {
-    size(WIDTH, HEIGHT);
+function setup() {
+    console.log("setup");
+    createCanvas(WIDTH, HEIGHT);
     // noStroke();
 }
 
 var circleX = WIDTH/2;
 var circleY = HEIGHT/2;
 
-void draw() {
+function bound(x, min, max) {
+    return x > min ? (x < max ? x : max) : min;
+}
+
+function draw() {
+    updateNumRects();
     background(255);
-    if (mousePressed == true) {
-        circleX = mouseX;
-        circleY = mouseY;
+    if (mouseIsPressed == true) {
+        circleX = bound(mouseX, LEFT_RECT_EDGE, LEFT_RECT_EDGE + BAR_WIDTH);
+        // circleY = mouseY;
     }
     fill(255);
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < NUM_RECTS; i++) {
         rect(LEFT_RECT_EDGE + RECT_WIDTH*i, TOP_RECT_EDGE, RECT_WIDTH, RECT_HEIGHT);
     }
     fill(255, 0, 0);
     ellipse(circleX, circleY, 20, 20);
+}
+
+function mousePress() {
+    
+}
+
+function mouseReleased() {
+    
+}
+
+function updateNumRects() {
+    NUM_RECTS = document.getElementById("denominator").value;
+    if (NUM_RECTS > BAR_WIDTH/10) {
+        document.getElementById("denominator").value = BAR_WIDTH/10;
+        NUM_RECTS = BAR_WIDTH/10;
+    }
+    RECT_WIDTH = BAR_WIDTH/NUM_RECTS;
+    
+    
 }
