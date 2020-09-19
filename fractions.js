4fe1cd21fc9f348c11be73ea6f73d57738240928
duncanwedig <duncan.wedig@gmail.com>
2020-09-19 06:40:25 -0700

    fractions thing works but is ugly

--- fractions.js
+++ fractions.js
@@ -17,8 +17,9 @@ function setup() {
     // noStroke();
 }
 
+var circleGoalX = WIDTH/2;
 var circleX = WIDTH/2;
-var circleY = HEIGHT/2;
+const circleY = HEIGHT/2;
 
 function bound(x, min, max) {
     return x > min ? (x < max ? x : max) : min;
@@ -27,33 +28,61 @@ function bound(x, min, max) {
 function draw() {
     updateNumRects();
     background(255);
-    if (mouseIsPressed == true) {
-        circleX = bound(mouseX, LEFT_RECT_EDGE, LEFT_RECT_EDGE + BAR_WIDTH);
-        // circleY = mouseY;
-    }
     fill(255);
     for (var i = 0; i < NUM_RECTS; i++) {
         rect(LEFT_RECT_EDGE + RECT_WIDTH*i, TOP_RECT_EDGE, RECT_WIDTH, RECT_HEIGHT);
     }
-    fill(255, 0, 0);
-    ellipse(circleX, circleY, 20, 20);
+    updateCircle();
 }
 
-function mousePress() {
-    
+function isMouseOnCanvas() {
+    return mouseX > 0 && mouseX < WIDTH && mouseY > 0 && mouseY < HEIGHT;
+}
+
+function updateCircleGoal() {
+    var numerator = Math.round((mouseX - LEFT_RECT_EDGE) / RECT_WIDTH);
+    document.getElementById("numerator").value = numerator;
+    circleGoalX = (numerator * RECT_WIDTH) + LEFT_RECT_EDGE;
 }
 
 function mouseReleased() {
-    
+    if (isMouseOnCanvas()) {
+        updateCircleGoal();
+    }
+
+}
+
+function updateFilledRects() {
+    var numerator = Math.round(document.getElementById("numerator").value);
+
+
 }
 
 function updateNumRects() {
-    NUM_RECTS = document.getElementById("denominator").value;
-    if (NUM_RECTS > BAR_WIDTH/10) {
-        document.getElementById("denominator").value = BAR_WIDTH/10;
-        NUM_RECTS = BAR_WIDTH/10;
+    if (NUM_RECTS != document.getElementById("denominator").value) {
+        NUM_RECTS = document.getElementById("denominator").value;
+        if (NUM_RECTS > BAR_WIDTH/10) {
+            document.getElementById("denominator").value = BAR_WIDTH/10;
+            NUM_RECTS = BAR_WIDTH/10;
+        }
+        RECT_WIDTH = BAR_WIDTH/NUM_RECTS;
+        updateCircleGoal();
     }
-    RECT_WIDTH = BAR_WIDTH/NUM_RECTS;
     
     
+}
+
+function updateCircle() {
+    if (mouseIsPressed == true) {
+        circleGoalX = bound(mouseX, LEFT_RECT_EDGE, LEFT_RECT_EDGE + BAR_WIDTH);
+        // circleY = mouseY;
+    }
+        circleX += 0.5 * (circleGoalX - circleX);
+    
+    fill(255, 0, 0);
+    noStroke();
+    rect(LEFT_RECT_EDGE, TOP_RECT_EDGE, circleX - LEFT_RECT_EDGE, BAR_HEIGHT);
+    ellipse(circleX, circleY, 20, 20);
+    stroke(0);
+
 }
